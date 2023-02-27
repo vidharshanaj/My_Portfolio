4491215edf3852e8d0cf6b67f6ae6c9b77e0ae42
VIDHARSHANA J <vidharshanajs@gmail.com>
2023-02-27 19:11:22 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*****animation */
+var typed =new Typed(".typing",{
+    strings:["","Frontend Developer","Web Designer","Electrical Engineer"],
+    typeSpeed:60,
+    BackSpeed:40,
+    loop: true
+
+})
+/***Aside */
+
